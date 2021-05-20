c0d05f18c4f6f2c461124f2ae7a793bc289d5d45
Raphael Roberts <raphael.roberts48@gmail.com>
2021-05-20 15:49:08 -0500

    Debug print statements

--- order_take.c
+++ order_take.c
@@ -1,10 +1,12 @@
 #include "c2logic/builtins.h"
+#include "c2logic/io.h"
 #include "helpers/consts.h"
 #include "helpers/setup.c"
 #include "helpers/utils.c"
 extern struct MindustryObject cell1;
 extern struct MindustryObject vault1;
 extern struct MindustryObject unit;
+extern struct MindustryObject message1;
 
 double get_low_resource_int() {
 	double low_resource_int = read(cell1, OFFSET_RESOURCE_NEEDED);
@@ -24,20 +26,31 @@ double get_low_resource_int() {
 
 void main_inner() {
 	setup();
+	println("SETUP");
 	int this_vault_id = read(cell1, 0);
+	print("VAULT_ID ");
+	printdln(this_vault_id);
 	ubind("flare");
 	int flag = sensor(unit, "flag");
 	double ident = get_flag(flag, F_IDENT);
+	print("IDENT ");
+	printdln(ident);
 	double vx = sensor(vault1, "x");
 	double vy = sensor(vault1, "y");
 	if (ident != IDENT) {
+		println("WRONG IDENT");
 		return;
 	}
 	double mode = get_flag(flag, F_MODE);
+	print("MODE ");
+	printdln(mode);
 	if ((mode == LOADING) | (mode == CLAIMED)) {
+		println("WRONG MODE");
 		return;
 	}
 	double resource_int = get_low_resource_int();
+	print("RESOURCE ");
+	printdln(resource_int);
 	if (mode == IDLE) {
 		double count = count_from_resource_int(vault1, resource_int);
 		double storage = sensor(vault1, "itemCapacity");
@@ -45,9 +58,11 @@ void main_inner() {
 		if (count / storage > THRESHOLD_FULL) {
 			return;
 		}
+		println("NOT FULL");
 		// claim any idle units immediately
 		flag = mod_flag(flag, F_MODE, CLAIMED);
 		unit_flag(flag);
+		println("CLAIMED");
 		// set relevant parameters
 		flag = mod_flag(flag, F_RESOURCE, resource_int);
 		flag = mod_flag(flag, F_VAULT_ID, this_vault_id);
@@ -74,4 +89,5 @@ void main_inner() {
 }
 void main() {
 	main_inner();
+	printflush(message1);
 }
