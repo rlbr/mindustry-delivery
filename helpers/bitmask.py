b1832bd98d9455ced2eb50ee2d026de6543bbd40
Raphael Roberts <raphael.roberts48@gmail.com>
2021-05-19 22:29:41 -0500

    Add the script I have been using to figure this all out

---
+++ helpers/bitmask.py
@@ -0,0 +1,53 @@
+from enum import IntEnum
+
+length_vals = [0] * 4
+
+
+class lengths(IntEnum):
+    RESOURCE_INT = 0
+    MODE = 1
+    MOVING = 2
+    ID = 3
+
+
+length_vals[lengths.RESOURCE_INT] = 4
+length_vals[lengths.MODE] = 3
+length_vals[lengths.MOVING] = 1
+length_vals[lengths.ID] = 8
+# 0000 000 0 00000000
+# rrrr mmm v iiiiiiii
+end = sum(length_vals)
+ends = []
+for length in length_vals:
+    end -= length
+    ends.append(end)
+
+
+def get_mask(length):
+    return (1 << length) - 1
+
+
+def read_flag(flag, index):
+    val = flag >> ends[index]
+    return val & get_mask(length_vals[index])
+
+
+def modify_flag(flag, index, val):
+    mask = get_mask(length_vals[index]) << ends[index]
+    mask = ~mask
+    breakpoint()
+    flag = flag & mask
+    val = val << ends[index]
+    return flag | val
+
+
+class int_array(list):
+    def __init__(self, lengths):
+        self.lengths = tuple(lengths)
+        self.flag = 0
+
+    def __getitem__(self, index):
+        return read_flag(self.flag, index)
+
+    def __setitem__(self, index, val):
+        self.flag = modify_flag(self.flag, index, val)
