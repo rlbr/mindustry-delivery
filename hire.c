5b53afffd6cfac4439c07785692e261a89b1a0fa
Raphael Roberts <raphael.roberts48@gmail.com>
2021-05-19 22:05:55 -0500

    Added "hire" component that finds idle flares and puts them to work.

---
+++ hire.c
@@ -0,0 +1,22 @@
+#include "c2logic/builtins.h"
+#include "c2logic/io.h"
+#include "consts.h"
+#include "setup.c"
+#include "utils.c"
+extern struct MindustryObject cell1;
+extern struct MindustryObject unit;
+extern struct MindustryObject message2;
+void main() {
+	setup();
+	ubind("flare");
+	int flag = sensor(unit, "flag");
+	int ident = get_flag(flag, F_IDENT);
+	if (ident != IDENT) {
+		flag = mod_flag(flag, F_IDENT, IDENT);
+		printdln(flag);
+		flag = mod_flag(flag, F_MODE, FULL);
+		printdln(flag);
+		printflush(message2);
+		unit_flag(flag);
+	}
+}
