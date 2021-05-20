8f3bd27093c6a3eb69e16412b33f2bc4cb2f0476
Raphael Roberts <raphael.roberts48@gmail.com>
2021-05-20 16:29:35 -0500

    Fixed deadlock

--- helpers/setup.c
+++ helpers/setup.c
@@ -11,7 +11,7 @@ int compute_write_mask(int length, int _end) {
 	return ~mask;
 }
 void setup() {
-	if (!read(cell1, 0)) {
+	while (!read(cell1, 0)) {
 		int index;
 		// change for what resources should be carried to storage vault
 		index = 0;
@@ -47,6 +47,8 @@ void setup() {
 			write(compute_write_mask(length, _end), cell1, OFFSET_WRITE_MASK + i);
 		}
 		// mark as configured
-		write(ceil(rand(1023)), cell1, 0);
+		if (read(cell1, OFFSET_RESOURCE_NEEDED) == PHASE_FABRIC) {
+			write(ceil(rand(1023)), cell1, 0);
+		}
 	}
 }
