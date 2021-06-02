a75480f0495ab1094aee79a08b1ca5036b1c145d
Raphael Roberts <raphael.roberts48@gmail.com>
2021-06-02 16:49:01 -0500

    Made resources an Enum

--- helpers/bitmask.py
+++ helpers/bitmask.py
@@ -2,22 +2,25 @@ from enum import IntEnum
 import functools
 import operator
 
-LEAD = 0
-COPPER = 1
-COAL = 2
-SAND = 3
-SCRAP = 4
-THORIUM = 5
-TITANIUM = 6
-SPORE_POD = 7
-# derived resources
-PYRATITE = 8
-SURGE_ALLOY = 9
-PHASE_FABRIC = 10
-PLASTANIUM = 11
-SILICON = 12
-GRAPHITE = 13
-METAGLASS = 14
+
+class resources(IntEnum):
+    LEAD = 0
+    COPPER = 1
+    COAL = 2
+    SAND = 3
+    SCRAP = 4
+    THORIUM = 5
+    TITANIUM = 6
+    SPORE_POD = 7
+    # derived resources
+    PYRATITE = 8
+    SURGE_ALLOY = 9
+    PHASE_FABRIC = 10
+    PLASTANIUM = 11
+    SILICON = 12
+    GRAPHITE = 13
+    METAGLASS = 14
+
 
 length_vals = [0] * 4
 
