059d98be3acbde503b4e61f63307be090f9a6bf1
Raphael Roberts <raphael.roberts48@gmail.com>
2021-06-01 19:54:01 -0500

    Added stupid simple build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+find . -maxdepth 1 -mindepth 1 -name "*.c" -print | while read c_file; do
+    outname=$(basename "${c_file}" | sed 's/c$/masm/g')
+    c2logic -O 2 -o "output/${outname}" "${c_file}"
+done
