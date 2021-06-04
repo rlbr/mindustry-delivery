6e898169a0f7781d8c29731f1112758a2acdefb7
Raphael Roberts <raphael.roberts48@gmail.com>
2021-06-03 23:00:16 -0500

    Make output directory if it doesn't exist

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+mkdir output
 find . -maxdepth 1 -mindepth 1 -name "*.c" -print | while read c_file; do
     outname=$(basename "${c_file}" | sed 's/c$/masm/g')
     c2logic -O 2 -o "output/${outname}" "${c_file}"
